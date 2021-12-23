11c9c146b0957d0c7ad7cfa9435fc0a4987d4590
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-12-23 14:40:25 +0100

    add custom hook

---
+++ src/hooks/index.js
@@ -0,0 +1,77 @@
+import { useState, useEffect, useRef } from "react";
+import { sortMovieByDate } from "../utils";
+import axios from "axios";
+
+/**
+ * custom hooks to fetch list movie
+ * @returns an object of the loading state, error state and sorted movie list
+ */
+export const useSwapiApi = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setErrors] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMovies() {
+    setIsLoading(true);
+
+    axios
+      .get("https://swapi.dev/api/films/")
+      .then((response) => {
+        const sortedList = sortMovieByDate(response.data.results);
+        setMovies(sortedList);
+        setIsLoading(false);
+      })
+      .catch((error) => setErrors(error));
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [hasError]);
+
+  return { isLoading, setIsLoading, hasError, movies };
+};
+
+export const useSelectedMovie = ({ selected }) => {
+  const [isLoadingCharacters, setLoadingCharacters] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [fetchCharacterError, setFetchCharacterError] = useState(false);
+  const previousSelections = useRef([]);
+
+  const charactersUrl = selected ? selected.characters : [];
+
+  // const alreadyFetched = previousSelections.current.filter((title) => title === selected.title);
+  // console.log("===>", alreadyFetched);
+
+  useEffect(() => {
+    setLoadingCharacters(true);
+
+    // if (alreadyFetched.length > 0) {
+    //     setCharacters(alreadyFetched[0]);
+    //     console.log('xxxx')
+    // }
+
+    if (charactersUrl.length > 0) {
+      const req = charactersUrl.map((url) =>
+        axios.get(url).then((response) => {
+          return response.data;
+        })
+      );
+
+      Promise.all(req)
+        .then((responses) => {
+          setCharacters(responses);
+          setLoadingCharacters(false);
+
+          previousSelections.current.push({
+            title: selected.title,
+            characters: responses,
+          });
+        })
+        .catch((error) => {
+          setFetchCharacterError(error);
+        });
+    }
+  }, [charactersUrl, fetchCharacterError, selected]);
+
+  return { isLoadingCharacters, fetchCharacterError, characters };
+};

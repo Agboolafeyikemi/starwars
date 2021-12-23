851f501ea794c7b386cc19431dfc7a7080563041
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2021-12-23 14:42:06 +0100

    opencrawl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { useSwapiApi, useSelectedMovie } from "./hooks";
+import logo from "./assets/starwars-logo.png";
+import "./App.css";
+
+import MovieDropDown from "./components/MovieDropDown";
+import FilterableTable from "./components/FilterableTable";
+import OpenCrawl from "./components/OpenCrawl";
 
 function App() {
+  const { isLoading, hasError, movies } = useSwapiApi();
+  const selectedMovie = useRef(null);
+  const [showOpenCrawl, setShowOpenCrawl] = useState(false);
+
+  const { isLoadingCharacters, fetchCharacterError, characters } =
+    useSelectedMovie({
+      selected: selectedMovie.current ? selectedMovie.current : null,
+    });
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (selectedMovie.current) {
+        setShowOpenCrawl(false);
+      }
+    }, 3000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [characters, showOpenCrawl]);
+
+  const handleMovieSelection = (e) => {
+    const selectedTitle = e.target.value;
+    const movie = movies.find((m) => m.title === selectedTitle);
+    selectedMovie.current = movie;
+    setShowOpenCrawl(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="main">
+        {(hasError || fetchCharacterError) && (
+          <div id="error">Something went wrong. Kindly refresh the page </div>
+        )}
+        {showOpenCrawl ? (
+          <OpenCrawl data={selectedMovie} />
+        ) : (
+          <>
+            <img id="logo" src={logo} alt="Star Wars logo" />
+            {!isLoadingCharacters && (
+              <button id="playCrawl" onClick={() => setShowOpenCrawl(true)}>
+                Play Crawl
+              </button>
+            )}
+
+            <MovieDropDown
+              isLoading={isLoading}
+              onChange={handleMovieSelection}
+              data={movies}
+              value={
+                selectedMovie.current ? selectedMovie.current.title : "Select"
+              }
+            />
+
+            {!isLoadingCharacters && <FilterableTable data={characters} />}
+          </>
+        )}
+      </div>
     </div>
   );
 }
